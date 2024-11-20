3e1b8df29324d4701dcbaf1bfe2b1cefabce8a81
xavierchanth <xchanthavong@gmail.com>
2024-11-20 08:54:03 -0500

    feat: button status for onboarding

--- packages/dart/npt_flutter/lib/features/onboarding/widgets/onboarding_button.dart
+++ packages/dart/npt_flutter/lib/features/onboarding/widgets/onboarding_button.dart
@@ -39,9 +39,14 @@ class OnboardingButton extends StatefulWidget {
   State<OnboardingButton> createState() => _OnboardingButtonState();
 }
 
+enum _OnboardingButtonStatus {
+  ready,
+  picking,
+  processingFile,
+}
+
 class _OnboardingButtonState extends State<OnboardingButton> {
-  //
-  BuildContext get appContext => App.navState.currentContext!;
+  _OnboardingButtonStatus buttonStatus = _OnboardingButtonStatus.ready;
 
   // TODO: when an atSign is being onboarded
   // make this button go into a loading state or show some visual indication
@@ -49,20 +54,24 @@ class _OnboardingButtonState extends State<OnboardingButton> {
   @override
   Widget build(BuildContext context) {
     final strings = AppLocalizations.of(context)!;
-    return ElevatedButton.icon(
-      onPressed: () async {
-        bool shouldOnboard = await selectAtsign();
-        if (shouldOnboard && context.mounted) {
-          var atsignInformation = context.read<OnboardingCubit>().state;
-          onboard(atsign: atsignInformation.atSign, rootDomain: atsignInformation.rootDomain);
-        }
-      },
-      icon: PhosphorIcon(PhosphorIcons.arrowUpRight()),
-      label: Text(
-        strings.getStarted,
-      ),
-      iconAlignment: IconAlignment.end,
-    );
+    return switch (buttonStatus) {
+      _OnboardingButtonStatus.ready => ElevatedButton.icon(
+          onPressed: () async {
+            bool shouldOnboard = await selectAtsign();
+            if (shouldOnboard && context.mounted) {
+              var atsignInformation = context.read<OnboardingCubit>().state;
+              onboard(atsign: atsignInformation.atSign, rootDomain: atsignInformation.rootDomain);
+            }
+          },
+          icon: PhosphorIcon(PhosphorIcons.arrowUpRight()),
+          label: Text(
+            strings.getStarted,
+          ),
+          iconAlignment: IconAlignment.end,
+        ),
+      _OnboardingButtonStatus.picking => const Text("Waiting for file to be picked"),
+      _OnboardingButtonStatus.processingFile => const Text("Processing file"),
+    };
   }
 
   Future<bool> selectAtsign() async {
@@ -141,7 +150,9 @@ class _OnboardingButtonState extends State<OnboardingButton> {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             backgroundColor: Colors.red,
-            content: Text(AppLocalizations.of(context)!.onboardingError),
+            content: Text(
+              onboardingResult?.message ?? AppLocalizations.of(context)!.onboardingError,
+            ),
           ),
         );
         break;
@@ -155,16 +166,9 @@ class _OnboardingButtonState extends State<OnboardingButton> {
     try {
       status = await util.atServerStatus(atsign);
     } catch (_) {
-      if (!mounted) return null;
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.red,
-          // TODO: new localization string
-          // "Failed to retrieve the atserver status"
-          content: Text(AppLocalizations.of(context)!.onboardingError),
-        ),
+      return AtOnboardingResult.error(
+        message: "Failed to retrieve the atserver status",
       );
-      return null;
     }
     AtOnboardingResult? result;
     if (!mounted) return null;
@@ -182,17 +186,10 @@ class _OnboardingButtonState extends State<OnboardingButton> {
             ),
           ),
         );
-        if (result == null && mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              backgroundColor: Colors.red,
-              // TODO: new localization string
-              // "There was an error activating your atSign, please try again later."
-              // "If the issue persists, please contact support"
-              content: Text(AppLocalizations.of(context)!.onboardingError),
-            ),
-          );
-        }
+        result ??= AtOnboardingResult.error(
+          message: "There was an error activating your atSign, please try again later.\n"
+              "If the issue persists, please contact support",
+        );
       case AtSignStatus.activated:
         // NOTE: for now this is hard coded to do atKey file upload
         // Later on, we can add the APKAM flow, and will need to make some
@@ -200,32 +197,17 @@ class _OnboardingButtonState extends State<OnboardingButton> {
         Stream<FileUploadStatus> statusStream = util.uploadAtKeysFile(atsign);
         result = await handleFileUploadStatusStream(statusStream, atsign);
       case AtSignStatus.notFound:
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            backgroundColor: Colors.red,
-            // TODO: new localization string
-            // "The atSign you have requested, doesn't exist in this root domain"
-            content: Text(AppLocalizations.of(context)!.onboardingError),
-          ),
+        result = AtOnboardingResult.error(
+          message: "The atSign you have requested, doesn't exist in this root domain",
         );
       case AtSignStatus.unavailable:
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            backgroundColor: Colors.red,
-            // TODO: new localization string
-            // "The atSign is unavailable, make sure you have internet connectivity"
-            content: Text(AppLocalizations.of(context)!.onboardingError),
-          ),
+        result = AtOnboardingResult.error(
+          message: "The atSign is unavailable, make sure you have internet connectivity",
         );
       case null: // This case should never happen, treat it as an error
       case AtSignStatus.error:
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            backgroundColor: Colors.red,
-            // TODO: new localization string
-            // "Failed to retrieve the atserver status"
-            content: Text(AppLocalizations.of(context)!.onboardingError),
-          ),
+        result = AtOnboardingResult.error(
+          message: "Failed to retrieve the atserver status",
         );
     }
     return result;
@@ -233,54 +215,73 @@ class _OnboardingButtonState extends State<OnboardingButton> {
 
   Future<AtOnboardingResult?> handleFileUploadStatusStream(Stream<FileUploadStatus> statusStream, String atsign) async {
     AtOnboardingResult? result;
+    outer:
     await for (FileUploadStatus status in statusStream) {
-      /// TODO some nice progress notifications:
-      /// Example implementation:
-      /// https://github.com/atsign-foundation/at_widgets/blob/b4006854fa93c21eeb5bcea41044787bdf0f6f32/packages/at_onboarding_flutter/lib/src/screen/at_onboarding_home_screen.dart#L659
+      // Don't return from inside this switch other wise the buttonStatus
+      // won't be reset to ready state
       switch (status) {
         case ErrorIncorrectKeyFile():
           result = AtOnboardingResult.error(
             message: "Invalid atKeys file detected",
           );
+          break outer;
         case ErrorAtSignMismatch():
           result = AtOnboardingResult.error(
             message: "The atKeys file you uploaded did not match the atSign requested",
           );
+          break outer;
         case ErrorFailedFileProcessing():
-          return AtOnboardingResult.error(
+          result = AtOnboardingResult.error(
             message: "Failed to process the atKeys file",
           );
+          break outer;
         case ErrorAtServerUnreachable():
           result = AtOnboardingResult.error(
             message: "Unable to connect to the atServer, make sure you have a stable internet connection",
           );
+          break outer;
         case ErrorAuthFailed():
           result = AtOnboardingResult.error(
             message: "Authentication failed",
           );
+          break outer;
         case ErrorAuthTimeout():
           result = AtOnboardingResult.error(
             message: "Authentication timed out",
           );
+          break outer;
         case ErrorPairedAtsign _:
           result = AtOnboardingResult.error(
             message: "The atSign ${status.atSign ?? atsign} is already paired, please contact support.",
           );
-        // We may not want to do anything in these cases...
-        // If we want to show some visual indications of progress, we can
-        // but it's not necessary
+          break outer;
         case FilePickingInProgress():
-        case FilePickingDone():
+          setState(() {
+            buttonStatus = _OnboardingButtonStatus.picking;
+          });
+          break; // don't break outer, this is a mid progress update
         case ProcessingAesKeyInProgress():
+          setState(() {
+            buttonStatus = _OnboardingButtonStatus.processingFile;
+          });
+          break; // don't break outer, this is a mid progress update
+
+        // We don't really need to handle these
+        case FilePickingDone():
         case ProcessingAesKeyDone():
           break;
 
         case FilePickingCanceled():
-          return AtOnboardingResult.cancelled();
+          result = AtOnboardingResult.cancelled();
+          break outer;
         case FileUploadAuthSuccess _:
-          return AtOnboardingResult.success(atsign: status.atSign ?? atsign);
+          result = AtOnboardingResult.success(atsign: status.atSign ?? atsign);
+          break outer;
       }
     }
+    setState(() {
+      buttonStatus = _OnboardingButtonStatus.ready;
+    });
     return result;
   }
 }
