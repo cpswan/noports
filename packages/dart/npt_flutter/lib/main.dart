d943d654ba9029c613699506750fd409b441fdc8
XavierChanth <xchanthavong@gmail.com>
2024-12-04 17:52:29 -0500

    chore: set app to regular Window mode when on MacOS

--- packages/dart/npt_flutter/lib/main.dart
+++ packages/dart/npt_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:npt_flutter/constants.dart';
 import 'package:window_manager/window_manager.dart';
@@ -7,10 +9,11 @@ import 'app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  var windowOptions = const WindowOptions(
+  var windowOptions = WindowOptions(
     title: "NoPorts Desktop",
     minimumSize: Constants.kWindowsMinWindowSize,
-    skipTaskbar: true,
+    skipTaskbar: Platform.isWindows,
+    alwaysOnTop: true,
   );
   windowManager.ensureInitialized();
   windowManager.waitUntilReadyToShow(windowOptions);
