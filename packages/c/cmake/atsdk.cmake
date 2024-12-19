7baabe236660593114b8cc96ad288a7d6739e5e5
XavierChanth <xchanthavong@gmail.com>
2024-12-19 09:23:09 -0500

    fix: stable monitor connections in c sshnpd

--- packages/c/cmake/atsdk.cmake
+++ packages/c/cmake/atsdk.cmake
@@ -3,7 +3,7 @@ if(NOT atsdk_FOUND)
   FetchContent_Declare(
     atsdk
     GIT_REPOSITORY https://github.com/atsign-foundation/at_c.git
-    GIT_TAG 9b9cb315a8af08a19e9679c9dad98572536f2ee4
+    GIT_TAG c37093b8b6ab4bf97f277f9752ef1975e67ed762
   )
   FetchContent_MakeAvailable(atsdk)
   install(TARGETS atclient atchops atlogger)
