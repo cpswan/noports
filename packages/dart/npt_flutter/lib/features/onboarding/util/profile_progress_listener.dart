e56c80085fdfd86e3475f91e70daff64f60866e4
Curtly Critchlow <curtlycritchlow@ccwc.io>
2025-01-29 09:54:50 -0400

    fix: sync notice shown to user when app is out of sync.

--- packages/dart/npt_flutter/lib/features/onboarding/util/profile_progress_listener.dart
+++ packages/dart/npt_flutter/lib/features/onboarding/util/profile_progress_listener.dart
@@ -5,9 +5,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:npt_flutter/app.dart';
 import 'package:npt_flutter/features/profile_list/bloc/profile_list_bloc.dart';
 
+import '../../profile_list/cubit/sync_cubit.dart';
+
 class ProfileProgressListener extends SyncProgressListener {
   @override
-  void onSyncProgressEvent(SyncProgress syncProgress) {
+  void onSyncProgressEvent(SyncProgress syncProgress) async {
+    await App.navState.currentContext!.read<SyncCubit>().checkSync();
     final profileListBlock = App.navState.currentContext!.read<ProfileListBloc>();
 
     if (syncProgress.syncStatus == SyncStatus.success &&
@@ -15,6 +18,7 @@ class ProfileProgressListener extends SyncProgressListener {
             (profileListBlock.state as ProfileListLoaded).profiles.isEmpty)) {
       profileListBlock.add(const ProfileListLoadEvent());
       log('ProfileProgressListener: ProfileListLoadEvent triggered to reload profiles');
+      await App.navState.currentContext!.read<SyncCubit>().checkSync();
     }
   }
 }
