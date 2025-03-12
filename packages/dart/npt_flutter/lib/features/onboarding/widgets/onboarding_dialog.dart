13d26add343c15df683a765064b4b2864eb7d90b
Curtly Critchlow <curtlycritchlow@ccwc.io>
2025-03-12 17:33:37 -0400

    fix: what is an atsign section started.

--- packages/dart/npt_flutter/lib/features/onboarding/widgets/onboarding_dialog.dart
+++ packages/dart/npt_flutter/lib/features/onboarding/widgets/onboarding_dialog.dart
@@ -1,80 +1,184 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:npt_flutter/features/onboarding/onboarding.dart';
 import 'package:npt_flutter/features/onboarding/util/atsign_manager.dart';
 import 'package:npt_flutter/features/onboarding/widgets/at_directory_selector.dart';
 import 'package:npt_flutter/features/onboarding/widgets/atsign_selector.dart';
+import 'package:npt_flutter/styles/app_color.dart';
 import 'package:npt_flutter/styles/sizes.dart';
 import 'package:npt_flutter/util/form_validator.dart';
 import 'package:npt_flutter/widgets/custom_container.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
-class OnboardingDialog extends StatelessWidget {
+class OnboardingDialog extends StatefulWidget {
   const OnboardingDialog({required this.options, super.key});
   final Map<String, AtsignInformation> options;
 
   @override
+  State<OnboardingDialog> createState() => _OnboardingDialogState();
+}
+
+class _OnboardingDialogState extends State<OnboardingDialog> {
+  bool visibility = false;
+
+  @override
   Widget build(BuildContext context) {
     final strings = AppLocalizations.of(context)!;
-    final width = MediaQuery.of(context).size.width * 0.50;
+    final width = MediaQuery.of(context).size.width * 0.60;
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
     return AlertDialog(
       backgroundColor: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(Sizes.p10),
       ),
-      content: Padding(
-        padding: const EdgeInsets.all(Sizes.p20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              width: width,
-              child: CustomContainer.background(
+      content: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.p20),
+          child: Column(
+            spacing: Sizes.p10,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CustomContainer.background(
+                width: width,
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(strings.selectorTitleAtsign),
-                    gapH16,
-                    AtsignSelector(
-                      options: options,
+                    Text(
+                      strings.selectorTitleAtsign,
+                      style: titleStyle!.copyWith(color: Colors.black),
+                    ),
+                    Text(
+                      strings.selectorSubTitleAtsign,
                     ),
                     gapH16,
-                    Text(strings.selectorTitleRootDomain),
-                    AtDirectorySelector(
-                      options: options,
+                    AtsignSelector(
+                      options: widget.options,
                     ),
                   ],
                 ),
               ),
-            ),
-            gapH10,
-            BlocBuilder<OnboardingCubit, OnboardingState>(builder: (context, state) {
-              return SizedBox(
+              CustomContainer.background(
                 width: width,
-                child: CustomContainer.background(
-                    child: Row(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(false);
-                      },
-                      child: Text(strings.cancel),
+                    Row(
+                      children: [
+                        Icon(
+                          PhosphorIcons.info(),
+                          color: AppColor.primaryColor,
+                        ),
+                        gapW14,
+                        Text(strings.whatIsClientAtsign, style: const TextStyle(color: AppColor.primaryColor)),
+                        const Spacer(),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              visibility = !visibility;
+                            });
+                          },
+                          icon: Icon(PhosphorIcons.caretDown()),
+                          color: AppColor.primaryColor,
+                        )
+                      ],
                     ),
-                    const Spacer(),
-                    ElevatedButton(
-                      onPressed: FormValidator.validateRequiredAtsignField(state.atSign) == null
-                          ? () {
-                              Navigator.of(context).pop(true);
-                            }
-                          : null,
-                      child: Text(strings.next),
+                    visibility ? gapH14 : gap0,
+                    Visibility(
+                      maintainAnimation: true,
+                      maintainState: true,
+                      visible: visibility,
+                      child: Row(
+                        children: [
+                          CustomContainer.foreground(
+                            padding: Sizes.p16,
+                            width: width / 2,
+                            child: Column(
+                              children: [
+                                Text(
+                                  strings.clientAtsignDescription,
+                                  textAlign: TextAlign.center,
+                                  style: titleStyle.copyWith(
+                                      fontSize: Sizes.p18, color: Colors.black, fontWeight: FontWeight.w500),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(strings.myNoPortsMsg),
+                                    IconButton(
+                                      icon: Icon(
+                                        PhosphorIcons.arrowUpRight(),
+                                        color: AppColor.primaryColor,
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                                Material(
+                                  elevation: 5,
+                                  child: SvgPicture.asset(
+                                    'assets/my_noports.svg',
+                                    height: 70,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
-                )),
-              );
-            })
-          ],
+                ),
+              ),
+              CustomContainer.background(
+                width: width,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      strings.selectorTitleRootDomain,
+                      style: titleStyle.copyWith(color: Colors.black),
+                    ),
+                    Text(strings.selectorSubTitleRootDomain),
+                    gapH16,
+                    AtDirectorySelector(
+                      options: widget.options,
+                    ),
+                  ],
+                ),
+              ),
+              BlocBuilder<OnboardingCubit, OnboardingState>(builder: (context, state) {
+                return SizedBox(
+                  width: width,
+                  child: CustomContainer.background(
+                      child: Row(
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                        child: Text(strings.cancel),
+                      ),
+                      const Spacer(),
+                      ElevatedButton(
+                        onPressed: FormValidator.validateRequiredAtsignField(state.atSign) == null
+                            ? () {
+                                Navigator.of(context).pop(true);
+                              }
+                            : null,
+                        child: Text(strings.next),
+                      ),
+                    ],
+                  )),
+                );
+              })
+            ],
+          ),
         ),
       ),
     );
--- packages/dart/npt_flutter/lib/widgets/custom_container.dart
+++ packages/dart/npt_flutter/lib/widgets/custom_container.dart
@@ -8,25 +8,27 @@ class CustomContainer extends StatelessWidget {
     required this.child,
     required this.color,
     required this.padding,
+    required this.width,
     super.key,
   });
 
-  const CustomContainer.background({required this.child, super.key})
+  const CustomContainer.background({required this.child, this.width, super.key})
       : color = AppColor.surfaceColor,
         padding = Sizes.p16;
 
-  const CustomContainer.foreground({required this.child, super.key})
-      : color = Colors.white,
-        padding = 0;
+  const CustomContainer.foreground({required this.child, this.width, super.key, this.padding = 0})
+      : color = Colors.white;
 
   final Widget child;
   final Color color;
   final double padding;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(padding),
+      width: width,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(Sizes.p10),
