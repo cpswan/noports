bc0a723ac7fc9e2800f68756e80e739df63713af
gkc <gary.casey@gmail.com>
2025-03-12 18:04:42 +0000

    test: update versions matrix for e2e tests

--- tests/e2e_all/scripts/main.sh
+++ tests/e2e_all/scripts/main.sh
@@ -49,8 +49,8 @@ atDirectoryPort=64
 testsToRun="all"
 
 # defaultDaemonVersions="c:current"
-defaultDaemonVersions="d:5.7.0 d:5.8.7 d:current c:current"
-defaultClientVersions="d:5.7.0 d:5.8.7 d:current"
+defaultDaemonVersions="d:5.5.0 d:5.8.7 d:current c:current"
+defaultClientVersions="d:5.5.0 d:5.8.7 d:current"
 
 daemonVersions=$defaultDaemonVersions
 clientVersions=$defaultClientVersions
