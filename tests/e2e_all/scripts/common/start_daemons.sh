e6daddd93182b01ca0d3ad5f17fb4b956adaf334
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-04-29 05:42:17 +0000

    feat: start_daemons.sh

--- tests/e2e_all/scripts/common/start_daemons.sh
+++ tests/e2e_all/scripts/common/start_daemons.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# TEMPORARY ----
+defaultDaemonVersions="d:4.0.5 d:5.2.0 d:5.5.0 d:current c:current"
+defaultClientVersions="d:4.0.5 d:5.2.0 d:5.5.0 d:current"
+
+daemonVersions=$defaultDaemonVersions
+clientVersions=$defaultClientVersions
+commitId="$(git rev-parse --short HEAD)"
+testScriptsDir=/home/jeremy/GitHub/noports/tests/e2e_all/scripts
+remoteUsername="atsign"
+source "$testScriptsDir/common/common_functions.include.sh"
+# END TEMPORARY ----
+
 if [ -z "$testScriptsDir" ]; then
   echo -e "    ${RED}check_env: testScriptsDir is not set${NC}" && exit 1
 fi
 
-source "$testScriptsDir/common/common_functions.include.sh"
-source "$testScriptsDir/common/check_env.include.sh" || exit $?
+# TEMPORARILY COMMENTING
+# source "$testScriptsDir/common/check_env.include.sh" || exit $?
 
 outputDir=$(getOutputDir)
 mkdir -p "${outputDir}/daemons"
@@ -32,45 +44,192 @@ waitUntilStarted() {
   done
 }
 
-# For each daemonVersion
-# Start two daemons for each typeAndVersion
-# 1) with the -u and -s flags set
-# 2) with neither of those flags set
-for typeAndVersion in $daemonVersions; do
-  logInfo "    Starting daemons for commitId $commitId and version $typeAndVersion"
+# Example usage:
+# buildDaemonDockerImage "../../dockerfiles/Dockerfile.dart.release" "noports-dart:v4.0.5" "release=v4.0.5"
+buildDaemonDockerImage() {
+  local type="$1"
+  local version="$2"
+  local dockerfilesDir="$(dirname "$0")/../../dockerfiles"
+  
+  echo "Building docker image for: Type: $type, Version: $version"
+  
+  if [[ "$type" == "d" || "$type" == "c" ]]; then
+    local language=""
+    if [[ "$type" == "d" ]]; then
+      language="dart"
+    else
+      language="c"
+    fi
 
-  pathToBinaries=$(getPathToBinariesForTypeAndVersion "$typeAndVersion")
+    local dockerfile=""
+    local tagName=""
+    local buildArg=""
 
-  cBinary="$pathToBinaries/sshnpd"
-  fRoot="--root-domain $atDirectoryHost"
-  fAtSigns="-m $clientAtSign -a $daemonAtSign"
-  extraFlags=""
-  if [[ $(versionIsAtLeast "$typeAndVersion" "d:5.3.0") == "true" ]]; then
-    apkamApp=$(getApkamAppName)
-    apkamDev=$(getApkamDeviceName "daemon" "$commitId")
-    keysFile=$(getApkamKeysFile "$daemonAtSign" "$apkamApp" "$apkamDev")
-    extraFlags="-k $keysFile"
+    if [[ "$version" == "current" ]]; then
+      dockerfile="$dockerfilesDir/Dockerfile.$language.current"
+      tagName="noports-$language:current"
+    else
+      dockerfile="$dockerfilesDir/Dockerfile.$language.release"
+      tagName="noports-$language:v$version" 
+      buildArg="release=v$version"
+    fi
+    
+    echo "Using: $dockerfile with tag: $tagName"
+    
+    local buildCommand=(sudo docker build --file "$dockerfile" --tag "$tagName" --quiet --target runtime)
+    
+    if [[ -n "$buildArg" ]]; then
+      buildCommand+=(--build-arg "$buildArg")
+    fi
+    
+    "${buildCommand[@]}" .
+    
+    local exitCode=$?
+    if [[ $exitCode -ne 0 ]]; then
+      echo "Error: Docker build failed with exit code $exitCode"
+    else
+      echo "Docker image built successfully: $tagName"
+    fi
+    
+    return $exitCode
+  else
+    echo "Error: Unknown type: $type"
+    return 1
   fi
+}
 
-  deviceName=$(getDeviceNameNoFlags "$commitId" "$typeAndVersion")
-  logFile="${outputDir}/daemons/${deviceName}.log"
-  logInfo "      Starting daemon version $typeAndVersion with neither the -u nor -s flags"
-  commandLine="$cBinary $fRoot $fAtSigns -d ${deviceName} --storage-path ${outputDir}/daemons/${deviceName}.storage -v $extraFlags"
-  echo "        --> $commandLine  >& $logFile 2>&1 &"
-  $commandLine >"$logFile" 2>&1 &
+# Example usage:
+# checkAndBuildDockerImage "d" "current"
+# checkAndBuildDockerImage "c" "4.0.5"
+checkAndBuildDockerImage() {
+  local type="$1"
+  local version="$2"
+  
+  if [[ "$type" == "d" || "$type" == "c" ]]; then
+    if [[ "$type" == "d" ]]; then
+      language="dart"
+    elif [[ "$type" == "c" ]]; then
+      language="c"
+    fi
 
-  waitUntilStarted $! "$deviceName" "$logFile"
-  echo
+    if [[ "$version" == "current" ]]; then
+      tagName="noports-$language:current"
+      echo "Always building current version: $tagName"
+      buildDocker=true
+    else
+      tagName="noports-$language:v$version"
+      
+      if sudo docker image inspect "$tagName" &>/dev/null; then
+        echo "Docker image $tagName already exists, skipping build"
+        buildDocker=false
+      else
+        echo "Docker image $tagName does not exist, will build"
+        buildDocker=true
+      fi
+    fi
 
-  deviceName=$(getDeviceNameWithFlags "$commitId" "$typeAndVersion")
-  logFile="${outputDir}/daemons/${deviceName}.log"
-  logInfo "      Starting daemon version $typeAndVersion with the -u and -s flags"
-  commandLine="$cBinary $fRoot $fAtSigns -d ${deviceName} --storage-path ${outputDir}/daemons/${deviceName}.storage -v -u -s $extraFlags"
-  echo "        --> $commandLine  >& $logFile 2>&1 &"
-  $commandLine >"$logFile" 2>&1 &
-  waitUntilStarted $! "$deviceName" "$logFile"
+    if [[ "$buildDocker" == true ]]; then
+      buildDaemonDockerImage "$type" "$version"
+      
+      local exitCode=$?
+      if [[ $exitCode -ne 0 ]]; then
+        echo "Error: Docker build failed with exit code $exitCode"
+        return $exitCode
+      else
+        echo "Docker build completed successfully for Type: $type, Version: $version"
+        return 0
+      fi
+    fi
+    
+    return 0
+  else
+    echo "Error: Unknown type: $type"
+    return 1
+  fi
+}
 
-  echo
-  echo
+# Example usage:
+# runDockerDaemon "d" "current" "dart" "@deviceName" "@clientAtSign" "@daemonAtSign" "-s" "noports-dart:current"
+runDockerDaemon() {
+  local type="$1"
+  local version="$2"
+  local language="$3"
+  local deviceName="$4"
+  local clientAt="$5"
+  local daemonAt="$6"
+  local flags="$7"
+  local tagName="$8"
+  
+  local flagsDesc=""
+  if [[ -n "$flags" ]]; then
+    flagsDesc="with $flags flags"
+  else
+    flagsDesc="with no flags"
+  fi
+  
+  echo "Starting container $flagsDesc, device name: $deviceName"
+  
+  local containerID=$(sudo docker run \
+    -d \
+    --rm \
+    -v "$HOME/.atsign/keys/:/atsign/.atsign/keys/" \
+    "$tagName" \
+    /bin/bash -c "sudo service ssh start && echo 'Running container for: Type: $type, Version: $version $flagsDesc' && /usr/local/bin/sshnpd -a $daemonAt -m $clientAt -d $deviceName $flags -v")
+  
+  local exitCode=$?
+  if [[ $exitCode -ne 0 ]]; then
+    echo "Error: Docker run failed with exit code $exitCode"
+    return $exitCode
+  else
+    echo "Container $flagsDesc started successfully, ID: $containerID"
+    return 0
+  fi
+}
 
+# Check if Docker image exists. If DNE, build Docker image
+# for typeAndVersion in $daemonVersions; do
+#   type=$(echo "$typeAndVersion" | cut -d':' -f1)
+#   version=$(echo "$typeAndVersion" | cut -d':' -f2)
+  
+#   checkAndBuildDockerImage "$type" "$version"
+#   if [[ $? -ne 0 ]]; then
+#     exit 1
+#   fi
+# done
+
+# Run Docker containers
+for typeAndVersion in $daemonVersions; do
+  type=$(echo "$typeAndVersion" | cut -d':' -f1)
+  version=$(echo "$typeAndVersion" | cut -d':' -f2)
+
+  if [[ "$type" == "d" || "$type" == "c" ]]; then
+    if [[ "$type" == "d" ]]; then
+      language="dart"
+    elif [[ "$type" == "c" ]]; then
+      language="c"
+    fi
+
+    if [[ "$version" == "current" ]]; then
+      tagName="noports-$language:current"
+    else
+      tagName="noports-$language:v$version"
+    fi
+
+    deviceName=$(getDeviceNameWithFlags "$commitId" "$typeAndVersion")
+    runDockerDaemon "$type" "$version" "$language" "$deviceName" "$clientAtSign" "$daemonAtSign" "-s" "$tagName"
+    
+    if [[ $? -ne 0 ]]; then
+      exit $?
+    fi
+
+    deviceName=$(getDeviceNameNoFlags "$commitId" "$typeAndVersion")
+    runDockerDaemon "$type" "$version" "$language" "$deviceName" "$clientAtSign" "$daemonAtSign" "" "$tagName"
+    
+    if [[ $? -ne 0 ]]; then
+      exit $?
+    fi
+  else
+    echo "Error: Unknown type: $type"
+    exit 1
+  fi
 done
