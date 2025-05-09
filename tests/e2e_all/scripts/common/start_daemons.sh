59e98d609dc16f4c0c7148ff0795fecd53670143
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-05-09 14:28:24 -0400

    feat: add `--no-cache` to buildDockerDaemon function in start_daemons.sh

--- tests/e2e_all/scripts/common/start_daemons.sh
+++ tests/e2e_all/scripts/common/start_daemons.sh
@@ -42,6 +42,7 @@ buildDockerDaemon() {
   local dockerBuildCommand="sudo docker build \
       -f \"$dockerfile\" \
       -t $tag \
+      --no-cache \
       $fBuildArg \
       --target runtime \
       ."
