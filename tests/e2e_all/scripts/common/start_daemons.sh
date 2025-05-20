62bdcee2511fbf16a715c74326a3da02a6628e3b
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-05-20 12:07:45 -0400

    feat: prune images

--- tests/e2e_all/scripts/common/start_daemons.sh
+++ tests/e2e_all/scripts/common/start_daemons.sh
@@ -124,6 +124,8 @@ runDockerDaemon() {
   eval "$dockerRunCommand"
 }
 
+sudo docker image prune -f
+
 buildDockerDaemonPids=()
 for typeAndVersion in $daemonVersions; do
   # typeAndVersion is a string like "d:4.0.5" or "c:current"
