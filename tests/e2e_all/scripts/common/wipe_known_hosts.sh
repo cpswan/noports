b2801d56520c77547e4447b1ae2d8993beda6c04
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-05-20 13:21:11 -0400

    feat: if macos, wipe `/var/root/.ssh/known_hosts`

--- tests/e2e_all/scripts/common/wipe_known_hosts.sh
+++ tests/e2e_all/scripts/common/wipe_known_hosts.sh
@@ -4,3 +4,7 @@ knownHostsFile="$HOME/.ssh/known_hosts"
 
 # Empty the known_hosts file
 echo "" >"$knownHostsFile"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    sudo sh -c 'echo "" > /var/root/.ssh/known_hosts'
+fi
