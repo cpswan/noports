483836cade3b7fcc30d20d8d18b7ec878b4a4bb5
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-05-20 11:24:24 -0400

    ci: change order

--- tests/e2e_all/scripts/common/run_tests.sh
+++ tests/e2e_all/scripts/common/run_tests.sh
@@ -44,8 +44,8 @@ if [[ $allowParallelization == "true" ]]; then
 
 
   listOfPids=()
-  for clientVersion in $clientVersions; do
-    for daemonVersion in $daemonVersions; do
+  for daemonVersion in $daemonVersions; do
+    for clientVersion in $clientVersions; do
       for testToRun in $testsToRun; do
         if [ "$testToRun" == "001_minus_s_flag" ]; then
           # Skip this test because it was already run above
