9831e85f9c5073857d62564fcce2f3efb39b63bc
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-05-21 11:18:59 -0400

    refactor: move start_daemons.sh functions to common_functions.include.sh

--- tests/e2e_all/scripts/common/start_daemons.sh
+++ tests/e2e_all/scripts/common/start_daemons.sh
@@ -7,125 +7,6 @@ fi
 source "$testScriptsDir/common/common_functions.include.sh"
 source "$testScriptsDir/common/check_env.include.sh" || exit $?
 
-outputDir=$(getOutputDir)
-mkdir -p "${outputDir}/daemons"
-
-# e.g. `buildDockerDaemon d 4.0.5``
-dockerfilesDir="$(dirname "$0")/../../dockerfiles"
-cd "$dockerfilesDir"/../../..
-buildDockerDaemon() {
-  local type="$1"
-  local version="$2"
-  
-  if [[ "$type" == "d" ]]; then
-      language="dart"
-  elif [[ "$type" == "c" ]]; then
-      language="c"
-  else
-      logErrorAndReport "Error: Unknown type: $type"
-      return 1
-  fi
-
-  if [[ "$version" == "current" ]]; then
-      dockerfile="$dockerfilesDir/Dockerfile.$language.current"
-      tag="noports-$type:current"
-      fBuildArg=""
-      fCache="--no-cache"
-  else
-      # assume "$version" is a release version like "4.0.5" or "5.2.0"
-      dockerfile="$dockerfilesDir/Dockerfile.$language.release"
-      tag="noports-$type:v$version"
-      fBuildArg="--build-arg release=v$version"
-      fCache=""
-  fi
-
-  logInfo "Building container for:      Type: $type, Version: $version"
-
-  local dockerBuildCommand="sudo docker build \
-      -f \"$dockerfile\" \
-      -t $tag \
-      --quiet \
-      $fCache \
-      $fBuildArg \
-      --target runtime \
-      ."
-  
-  logInfo "Executing Docker build command: $dockerBuildCommand"
-
-  local max_retries=3
-  local retry_count=0
-  local exitCode=1
-
-  while [[ $exitCode -ne 0 && $retry_count -lt $max_retries ]]; do
-    if [[ $retry_count -gt 0 ]]; then
-      logInfo "Retrying Docker build (attempt $((retry_count+1))/$max_retries)..."
-      sleep 1
-    fi
-    
-    eval "$dockerBuildCommand"
-    exitCode=$?
-    retry_count=$((retry_count+1))
-  done
-  
-  if [[ $exitCode -ne 0 ]]; then
-      logErrorAndReport "Error: Docker build failed with exit code $exitCode after $max_retries attempts"
-      return $exitCode
-  else
-      logInfo "Container $type $version built successfully"
-      return 0
-  fi
-}
-
-# usage: `waitUntilDockerDaemonStarted $logFile $timeout` 
-# - logFile is the path to the log file of the docker daemon
-# - timeout is optional, default is 60 seconds
-waitUntilDockerDaemonStarted() {
-  logFile="$1"
-  timeout="${2:-60}"
-
-  for i in $(seq 1 "$timeout"); do
-    if grep "monitor started" "$logFile" 2>/dev/null; then
-      return 0
-    fi
-    sleep 0.2
-  done
-
-  cat $logFile
-  
-  exit 1
-}
-
-# e.g. `runDockerDaemon "d" "4.0.5" "deviceName" "clientAtSign" "daemonAtSign" "log.txt" "-u -s"
-# e.g. `runDockerDaemon "c" "current" "deviceName" "clientAtSign" "daemonAtSign" "log.txt"`
-runDockerDaemon() {
-  local type="$1"
-  local version="$2"
-  local deviceName="$3"
-  local clientAt="$4"
-  local daemonAt="$5"
-  local daemonFlags="$6"
-
-  if [[ "$version" == "current" ]]; then
-    tag="noports-$type:current"
-  else
-    tag="noports-$type:v$version"
-  fi
-
-  logInfo "Starting container for: Type: $type, Version: $version, Flags: $daemonFlags, Device name: $deviceName, Client atSign: $clientAt, Daemon atSign: $daemonAt"
-
-  containerName="e2e_all-$deviceName"
-  local dockerRunCommand="sudo docker run \
-    --rm \
-    -d \
-    --name \"$containerName\" \
-    -v \"$testRuntimeDir/keys/:/atsign/.atsign/keys/\" \
-    \"$tag\" \
-    /bin/bash -c \"sudo service ssh start && /usr/local/bin/sshnpd -a $daemonAt -m $clientAt -d $deviceName $daemonFlags -v\""
-
-  logInfo "Executing: $dockerRunCommand"
-  eval "$dockerRunCommand"
-}
-
 buildAllDockerDaemons() {
   buildDockerDaemonPids=()
   for typeAndVersion in $daemonVersions; do
@@ -194,6 +75,13 @@ runAllDockerDaemons() {
     waitUntilDockerDaemonStarted "$logFile" 60
   done
 }
+
+outputDir=$(getOutputDir)
+mkdir -p "${outputDir}/daemons"
+
+# e.g. `buildDockerDaemon d 4.0.5``
+dockerfilesDir="$(dirname "$0")/../../dockerfiles"
+cd "$dockerfilesDir"/../../..
 
 buildAllDockerDaemons
 runAllDockerDaemons
